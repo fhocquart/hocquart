598527b8508d9c8de96461e8d702c030fac7ed06
fhocquart <152532095+fhocquart@users.noreply.github.com>
2024-02-28 07:04:51 -0500

    Update for.sh

--- for.sh
+++ for.sh
@@ -7,4 +7,13 @@ done
 
 sleep 2 
 
-echo "wow we made it to the world cup finals"
+echo "wow we made it to the champions league finals"
+
+for cups in {11..20};
+
+sleep 2 
+echo "for the tenth season the UCL has been won again by Real Madrid CF"
+
+sleep 1
+
+echo " Congratulations !!! and Hala Madrid"
